3d76e963e662ba288fa240c5c9ef2b111e7e9cc0
Fedonecchia <fedonecchia@gmail.com>
2017-08-17 22:17:50 +0200

    Add files via upload
    Major bug that prevented the code from running fixed

--- Battleship.py
+++ Battleship.py
@@ -455,32 +455,33 @@ while ready == "N":
 		mov = input("").upper()
 		if len(mov) > 1:
 			mov = input("Please only type one direction\n").upper()
-			if mov == "W":
-				if board1.index(shipsp1[num][0]) < 10:
-					printboard(1, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp1[num]:
-						shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)-10]
-			if mov == "S":
-				if board1.index(shipsp1[num][-1]) > 90:
-					printboard(1, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp1[num]:
-						shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)+10]
-			if mov == "A":
-				if board1.index(shipsp1[num][0]) == 1 or len(str(board1.index(shipsp1[num][-1]))) == 2 and str(board1.index(shipsp1[0]))[1] == 1:
-					printboard(1, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp1[num]:
-						shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)-1]
-			if mov == "D":
-				if board1.index(shipsp1[num][-1]) == 9 or len(str(board1.index(shipsp1[num][-1]))) == 2 and str(board1.index(shipsp1[num][-1]))[1] == "9":
-					printboard(1, 0)
-					mov = input("Can't go there!\n")
-				else:
+		if mov == "W":
+			if board1.index(shipsp1[num][0]) < 10:
+				printboard(1, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp1[num]:
+					shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)-10]
+		if mov == "S":
+			if board1.index(shipsp1[num][-1]) > 90:
+				printboard(1, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp1[num]:
+					shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)+10]
+		if mov == "A":
+			if board1.index(shipsp1[num][0]) == 1 or len(str(board1.index(shipsp1[num][-1]))) == 2 and str(board1.index(shipsp1[0]))[1] == 1:
+				printboard(1, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp1[num]:
+					shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)-1]
+		if mov == "D":
+			if board1.index(shipsp1[num][-1]) == 9 or len(str(board1.index(shipsp1[num][-1]))) == 2 and str(board1.index(shipsp1[num][-1]))[1] == "9":
+				printboard(1, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp1[num]:
 					shipsp1[num][shipsp1[num].index(piece)] = board1[board1.index(piece)+1]
 		check(p1)
 		printboard(1, 0)
@@ -503,37 +504,37 @@ while ready == "N":
 	movit = "Y"
 	while movit == "Y":
 		print("Where do you want to move ship number ", num+1, "? (Use WASD followed by Enter)\n", sep="")
-		mov = input("").upper()
-		if len(mov) > 1:
-			mov = input("Please only type one direction\n").upper()
-			if mov == "W":
-				if board2.index(shipsp2[num][0]) < 10:
-					printboard(2, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp2[num]:
-						shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)-10]
-			if mov == "S":
-				if board2.index(shipsp2[num][-1]) >= 90:
-					printboard(2, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp2[num]:
-						shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)+10]
-			if mov == "A":
-				if board2.index(shipsp2[num][0]) == 0 or len(str(board2.index(shipsp2[num][0]))) == 2 and str(board2.index(shipsp2[0]))[1] == "0":
-					printboard(2, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp2[num]:
-						shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)-1]
-			if mov == "D":
-				if board2.index(shipsp2[num][-1]) == 9 or len(str(board2.index(shipsp2[num][-1]))) == 2 and str(board2.index(shipsp2[num][-1]))[1] == "9":
-					printboard(2, 0)
-					mov = input("Can't go there!\n")
-				else:
-					for piece in shipsp2[num]:
-						shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)+1]
+	mov = input("").upper()
+	if len(mov) > 1:
+		mov = input("Please only type one direction\n").upper()
+		if mov == "W":
+			if board2.index(shipsp2[num][0]) < 10:
+				printboard(2, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp2[num]:
+					shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)-10]
+		if mov == "S":
+			if board2.index(shipsp2[num][-1]) >= 90:
+				printboard(2, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp2[num]:
+					shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)+10]
+		if mov == "A":
+			if board2.index(shipsp2[num][0]) == 0 or len(str(board2.index(shipsp2[num][0]))) == 2 and str(board2.index(shipsp2[0]))[1] == "0":
+				printboard(2, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp2[num]:
+					shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)-1]
+		if mov == "D":
+			if board2.index(shipsp2[num][-1]) == 9 or len(str(board2.index(shipsp2[num][-1]))) == 2 and str(board2.index(shipsp2[num][-1]))[1] == "9":
+				printboard(2, 0)
+				mov = input("Can't go there!\n")
+			else:
+				for piece in shipsp2[num]:
+					shipsp2[num][shipsp2[num].index(piece)] = board2[board2.index(piece)+1]
 		check(p2)
 		printboard(2, 0)
 		movit = input("\n\nDo you want to move it again? (y/n)\n").upper()
